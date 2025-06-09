8d0aebe8bb461e243e9dfd516784c8223f448510
Vinicius Henrique Cerrone <vinicius.cerrone@forlogic.net>
2025-06-09 11:55:03 -0300

    #96202

--- dashboard/script_modal.js
+++ dashboard/script_modal.js
@@ -33,6 +33,94 @@ function fecharModal() {
 
 // Carregar dados na tabela e configurar edição
 document.addEventListener('DOMContentLoaded', () => {
+
+    //barra de pesquisa do header:
+    const campoPesquisa = document.getElementById('campoPesquisa')
+    const resultadosPesquisa = document.getElementById('resultadosPesquisa')
+
+
+    campoPesquisa.addEventListener('input', function () {
+        const termo = this.value.toLowerCase().trim(); //para nao ter diferença entre maiusculas e minusculas
+        const usuarios = JSON.parse(localStorage.getItem('usuarios')) || [];
+
+        resultadosPesquisa.innerHTML = ''
+
+        if (!termo) {
+            resultadosPesquisa.style.display = 'none'
+            //quando a pesquisa estiver vazia, todas as tr voltam a ficar visíveis.
+            const linhas = tabela.getElementsByTagName('tr')
+            for (let i = 0; i < linhas.length; i++) {
+                linhas[i].style.display = '';
+            }
+
+            return;
+        }
+
+        const resultados = usuarios.filter(usuario =>
+            usuario.nome && usuario.nome.toLowerCase().includes(termo) // filter percorre a array e mantem resultados cujo nome contem o termo digitado
+        );
+
+        if (resultados.length === 0) {
+            resultadosPesquisa.style.display = 'none'
+            return;
+        }
+
+        //cria uma div dinamicamente e coloca o nome do usuario dentro dessa div
+        resultados.forEach(usuario => {
+            const item = document.createElement('div');
+            item.textContent = usuario.nome
+            item.style.padding = '8px';
+            item.style.cursor = 'pointer'
+
+            item.addEventListener('click', () => {
+                //configuração caso clique no resultado da pesquisa
+                const usuarios = JSON.parse(localStorage.getItem('usuarios')) || []
+                
+                const index = usuarios.findIndex(u =>
+                    u.nome === usuario.nome
+                    //isso ta estranho  
+                );
+
+                if(index !== -1){
+                    const modal = document.getElementById('modal_edicao')
+                    modal.dataset.index = index
+                    modal.classList.remove('oculto')
+                }
+
+                resultadosPesquisa.style.display = 'none'
+
+            });
+
+            resultadosPesquisa.appendChild(item); //adiciona ao resultadosPesquisa
+        });
+
+        resultadosPesquisa.style.display = 'block'
+
+        //Filtra a tabela com os usuarios que combinam com a barra de pesquisa
+        const linhas = tabela.getElementsByTagName('tr')
+        for (let i = 0; i < linhas.length; i++) {
+            const linha = linhas[i]
+            const nomeCelula = linha.cells[0]
+
+            if (nomeCelula) {
+                const nome = nomeCelula.textContent.toLowerCase()
+
+                if (nome.includes(termo)) {
+                    linha.style.display = '';
+                } else {
+                    linha.style.display = 'none'
+                }
+            }
+        }
+
+    });
+
+    document.addEventListener('click', function (e) {
+        if (!campoPesquisa.contains(e.target)) {
+            resultadosPesquisa.style.display = 'none'
+        }
+    });
+
     const tabela = document.querySelector('#tabela_usuarios tbody')
     if (tabela) {
         const usuarios = JSON.parse(localStorage.getItem('usuarios')) || []
@@ -295,5 +383,35 @@ document.addEventListener('DOMContentLoaded', () => {
             });
 
         }
+    }
+
+
+    //Excluir dados do localstorage
+
+    const botaoExcluir = document.getElementById('excluir')
+    if (botaoExcluir) {
+        botaoExcluir.addEventListener('click', () => {
+            if (!confirm("Quer excluir esse cadastro?")) {
+                return;
+            }
+
+            const modal = document.getElementById('modal_edicao');
+            const index = modal.dataset.index;
+            let usuarios = JSON.parse(localStorage.getItem('usuarios')) || [];
+
+            // Remove o usuário do array
+            usuarios.splice(index, 1);
+            localStorage.setItem('usuarios', JSON.stringify(usuarios));
+
+            modal.classList.add('oculto');
+
+            //atualiza o bloco
+            if (document.getElementById('bloco_1')) {
+                atualizarBlocos();
+            }
+
+            alert('Cadastro excluído');
+            location.reload();
+        });
     }
 });
